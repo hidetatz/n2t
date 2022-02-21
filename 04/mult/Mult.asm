174f5cb1e4edb7d42518e7037477aec2bc5faaee
Hidetatz Yaginuma <hidetatz@gmail.com>
2022-02-21 23:35:45 +0900

    impl Mult

--- 04/mult/Mult.asm
+++ 04/mult/Mult.asm
@@ -9,4 +9,43 @@
 // This program only needs to handle arguments that satisfy
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
-// Put your code here.
+// Adds R0 to R2 for R1 times
+
+	// initialize i (loop index) as 0
+	@i
+	M=0
+	// initialize R2 (a register to store the result) as 0
+	@2
+	M=0
+(LOOP)
+	// store i to D
+	@i
+	D=M
+	// now, D is i
+	// calculate current loop time
+	// i = i - R1 -> negative value if more loop is needed
+	@1
+	D=D-M
+
+	// Jump to END if no more loop is needed
+	@END
+	D;JGE
+
+	// load R0 to register
+	@0
+	D=M
+
+	// add loaded R0 to R2
+	@2
+	M=D+M
+
+	// increment i
+	@i
+	M=M+1
+
+	// loop again
+	@LOOP
+	0;JMP
+(END)
+	@END
+	0;JMP // finish program
